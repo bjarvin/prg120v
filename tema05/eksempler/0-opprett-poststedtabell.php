32219441e2935ca0d00ee9d2156cc4add8afad7f
bjarvin <gb@usn.no>
2025-05-07 09:48:47 +0200

    Update 0-opprett-poststedtabell.php

--- tema05/eksempler/0-opprett-poststedtabell.php
+++ tema05/eksempler/0-opprett-poststedtabell.php
@@ -1,16 +1,12 @@
-<?php  /* vis-alle-poststeder */
-/*
-/*  Programmet skriver ut alle registrerte poststeder
-*/  
+<?php  /* opprett poststedtabell */
 
-include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+include("db-tilkobling.php"); 
 
-
-  $sqlSetning="CREATE TABLE IF NOT EXISTS poststed (postnr CHAR(4) NOT NULL, poststed VARCHAR(30) NOT NULL, PRIMARY KEY (postnr));";
+$sqlSetning="CREATE TABLE IF NOT EXISTS poststed (postnr CHAR(4) NOT NULL, poststed VARCHAR(30) NOT NULL, PRIMARY KEY (postnr));";
    
-  $sqlResultat=mysqli_query($db,$sqlSetning) or die("ikke mulig  opprette tabell");
-    /* SQL-setning sendt til database-serveren */
+mysqli_query($db,$sqlSetning) or die("ikke mulig  opprette tabell");
+  /* SQL-setning sendt til database-serveren */
  
-  print("Poststedtabell opprettet (hvis den ikke var opprettet tidligere)");
+print("Poststedtabell opprettet (hvis den ikke var opprettet tidligere)");
 ?>
  
