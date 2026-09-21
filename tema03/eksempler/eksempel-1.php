18856af2934a204aef42c8040ff94b67165ee176
bjarvin <gb@usn.no>
2026-09-21 13:02:07 +0200

    Update eksempel-1.php

--- tema03/eksempler/eksempel-1.php
+++ tema03/eksempler/eksempel-1.php
@@ -1,10 +1,17 @@
 <?php     /* Eksempel 1 */
 /*
-/*    Programmet skriver ut tallene fra 1 til 10 p� hver sin linje
+/*    Programmet skriver ut tallene fra 1 til 10 på hver sin linje
 /*    Eksempelet viser bruk av for-setning
 */
   for ($tall=1;$tall<=10;$tall++)  
     {
       print("$tall <br/>");  
     }
+
+/*
+startverdi: $tall=1 
+betingelse: $tall<=10
+økning: $tall++ (dette er det samme som $tall=$tall+1)
+*/
+
 ?>
